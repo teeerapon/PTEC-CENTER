bd877f61f7399d0f28538e892b60dcf523572294
teerapon <the_trp@hotmail.com>
2024-01-16 11:43:15 +0700

    commit nac 16/01/2024

--- src/components/FixAssets/NAC_PURE/Documents/Appbar.js
+++ src/components/FixAssets/NAC_PURE/Documents/Appbar.js
@@ -53,7 +53,7 @@ export default function Nac_Main({ nac_code, nac_type, approveData, sendHeader }
                     {approveData.filter((res) => res.limitamount < sendHeader[0].sumPrice && sendHeader[0].nac_type !== 1).map((resMap) => (
                       <Typography style={{ 'color': resMap.status === 1 ? 'blue' : 'black', fontSize: '1rem', fontWeight: 'bold' }}>
                         {
-                          resMap.workflowlevel === 0 ? `(ตรวจสอบ BV: ${resMap.approverid})` :
+                          // resMap.workflowlevel === 0 ? `(ตรวจสอบ BV: ${resMap.approverid})` :
                             resMap.workflowlevel === 1 ? `(AM: ${resMap.approverid})` :
                               resMap.workflowlevel === 2 ? `(SM: ${resMap.approverid})` :
                                 resMap.workflowlevel === 3 ? `(DM: ${resMap.approverid})` :
