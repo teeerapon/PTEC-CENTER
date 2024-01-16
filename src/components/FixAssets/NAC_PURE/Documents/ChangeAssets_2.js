5c327895da4739d02bcf193cbd5d0fe2e1e5aa2d
teerapon <the_trp@hotmail.com>
2024-01-16 11:54:53 +0700

    commit center nac 16/01/2024 11:54

--- src/components/FixAssets/NAC_PURE/Documents/ChangeAssets_2.js
+++ src/components/FixAssets/NAC_PURE/Documents/ChangeAssets_2.js
@@ -527,7 +527,7 @@ export default function FormsStart() {
       const reqUpdateStatus = {
         usercode: data.UserCode,
         nac_code: nac_code,
-        nac_status: 3,
+        nac_status: approveData.filter((res) => res.approverid === data.UserCode && res.status === 0)[0] ? 2 : 3,
         nac_type: sendHeader[0].nac_type,
         source: sendHeader[0].source,
         sourceDate: sendHeader[0].sourceDate,
